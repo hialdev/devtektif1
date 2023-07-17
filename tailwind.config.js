7b51a99909159e267e62a27a3040b21239f67388
hialdev <al.developer12@gmail.com>
2023-07-17 12:16:23 +0700

    tailwind prefix added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dt-purple' : '#674188',
+      'dt-light' : '#F7EFE5',
+      'dt-dark' : '#1C1D1F',
+    }
   },
   plugins: [],
 }
