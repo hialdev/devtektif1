15c2724efbd52e18494aa8f3c11af8af47e4934c
hialdev <al.developer12@gmail.com>
2023-07-20 20:09:04 +0700

    tailwind prefix added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
@@ -9,6 +11,12 @@ module.exports = {
       'dt-purple' : '#674188',
       'dt-light' : '#F7EFE5',
       'dt-dark' : '#1C1D1F',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
     }
   },
   plugins: [],
